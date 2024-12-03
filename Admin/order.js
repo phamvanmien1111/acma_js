59346f13bf6994a2bfc3760e614a845a09cd5eb9
phamvanmien1111 <phamvanmien2001@gmail.com>
2024-12-03 14:32:58 +0700

    lưu Thay Đổi

---
+++ Admin/order.js
@@ -0,0 +1,40 @@
+const url = "http://localhost:3000/orders";
+
+fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+        data.forEach((order) => {
+            renderProduct(order);
+        });
+    });
+
+const orderTableBody = document.querySelector('#order'); 
+
+const renderProduct = (order) => {
+    order.products.forEach((product) => { 
+        const output = `
+            <tr>
+                <td
+                class="p-4 truncate overflow-hidden whitespace-nowrap"
+                style="width: 100px; max-width: 100px; cursor: pointer;"
+                onclick="showFullContent('${product.name}')"
+                >
+                ${product.name}
+                </td>
+
+                <td class="p-4  w-[190px]"><img src="${product.image}" alt="${product.name}" class="w-30 h-30"></td>
+                <td class="p-4">${product.price.toLocaleString()} VNĐ</td>
+                <td class="p-4">${product.quantity}</td>
+                <td class="p-4">${(product.price * product.quantity).toLocaleString()} VNĐ</td>
+                <td class="p-4">${new Date(order.orderDate).toLocaleString()}</td>
+                <td class="p-4">
+                    <button class="bg-red-500 text-white px-4 py-2 rounded" onclick="deleteOrder('${order.id}')">Xóa</button>
+                </td>
+            </tr>
+        `;
+        orderTableBody.insertAdjacentHTML("beforeend", output);
+    });
+}
+function showFullContent(content) {
+    alert(content); // Hiển thị nội dung đầy đủ trong alert
+  }
